d1362155ac8132216d8f7bb7b932517eb1973969
milan <milan.herbe@gmail.com>
2024-03-02 09:36:29 +0100

    Update Carte Working except IMG

--- components/carte.vue
+++ components/carte.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="h-96 w-80 text-left bg-blue-400 rounded">
+    <div class="h-96 w-80 text-left bg-neutral-400 rounded-lg backdrop-blur-sm opacity-50">
       <h1 class="m-4">{{ carte.title }}</h1>
-      <img :src="carte.image" />
+      <img :src="carte.image" class="m-4"/>
       <p class="min-h-10 m-4">Description : {{ carte.description }}</p>
       <h3 class="m-4">Mes outils utilisés : {{ carte.outils }}</h3>
     </div>
--- data/carteData.ts
+++ data/carteData.ts
@@ -3,19 +3,19 @@ import type { CarteInterface } from "~/interfaces/carte.interface";
 export default [
     {
         title: 'Projet 1',
-        image: './../images/php.png',
+        image: "portfolio/images/projetBoutique.png",
         outils: 'Figma, React, JS, CSS',
         description: 'Ce projet était basé sur une entreprise fictive "Flink" vendant des prestations de site Web.'
     },
     {
         title: 'Projet 2',
-        image: './../images/projetBoutique.png',
+        image: '@/images/projetBoutique.png',
         outils: 'Figma, Dyma, SCSS, Vue3, TS',
         description: "J'ai réalisé ce site à l'aide de la plateforme Dyma afin d'apprendre le langage Vue."
     },
     {
         title: 'Projet 3',
-        image: './../images/js.png',
+        image: '@/images/js.png',
         outils: 'Figma, JS, HTML, SCSS',
         description: "Ce projet était mon premier portfolio que j'ai pu effectué ainsi vous pouvez l'evolution."
     },
